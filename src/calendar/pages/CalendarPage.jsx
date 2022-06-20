6bf950f0e342b24fef037cc8a8a04a03b8530e7d
Enuel <enuel.monzon@gmail.com>
2022-06-20 02:39:33 -0300

    installation react-big-calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,47 @@
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import { addHours, format, parse, startOfWeek, getDay } from 'date-fns';
+import enUS from 'date-fns/locale/en-US'
+
 import { Navbar } from "../";
+
+const locales = {
+  'en-US': enUS,
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+const events = [{
+  title: 'Aprende React',
+  notes: 'React es un framework de JavaScript',
+  start: new Date(),
+  end: addHours( new Date(), 2),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Enuel',
+  }
+}]
 
 export const CalendarPage = () => {
   return (
     <>
       <Navbar />
+
+      <Calendar
+      localizer={ localizer }
+      events={ events }
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 'calc( 100vh - 80px )' }}
+    />
     </>
   );
 };
