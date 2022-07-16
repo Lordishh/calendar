bb312612d61e59fe7bc187cf67faebe757201fe4
Enuel <enuel.monzon@gmail.com>
2022-07-16 16:27:35 -0300

    agregando useForm hook

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,7 +3,7 @@ import { LoginPage } from "../auth";
 import { CalendarPage } from "../calendar";
 
 export const AppRouter = () => {
-  const authStatus = "autenticated"; // "not-authenticated";
+  const authStatus = 'not-authenticated'; //"autenticated"; // "not-authenticated";
 
   return (
     <Routes>
